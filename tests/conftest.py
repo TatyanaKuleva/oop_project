1418de65000cabd1605b4246c718fd2a8bab735d
tatyana kuleva <tatianaarsentievna@gmail.com>
2025-04-02 13:00:57 +0300

    create class and tests for class

---
+++ tests/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+
+from src.category import Category
+from src.product import Product
+
+
+@pytest.fixture
+def first_product():
+    return Product(name="first product", description="first product is a first product", price=3.0, quantity=10)
+
+
+@pytest.fixture
+def first_category():
+    return Category(
+        name="first category", description="first category is a first category", products=["first", "second", "third"]
+    )
---
+++ tests/test_category.py
@@ -0,0 +1,6 @@
+def test_category_init(first_category):
+    assert first_category.name == "first category"
+    assert first_category.description == "first category is a first category"
+    assert first_category.products == ["first", "second", "third"]
+    assert first_category.category_count == 1
+    assert first_category.product_count == 3
