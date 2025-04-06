17b100466e287fdef7a5d1f5d41c338037c23ea6
tatyana kuleva <tatianaarsentievna@gmail.com>
2025-04-06 17:50:31 +0300

    add tests

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,6 +1,20 @@
+from src.category import Category
+
+
 def test_category_init(first_category):
     assert first_category.name == "first category"
     assert first_category.description == "first category is a first category"
-    assert first_category.products == ["First", "Second", "Third"]
+    assert first_category.products == ("First product, 3.0руб. Остаток: 10 шт.\n" "Second product, 5.0руб. Остаток: 20 шт.\n")
     assert first_category.category_count == 1
+    assert first_category.product_count == 2
+
+def test_list_product_property(first_category):
+    assert first_category.products == ("First product, 3.0руб. Остаток: 10 шт.\n" "Second product, 5.0руб. Остаток: 20 шт.\n")
+
+def test_list_product_setter(first_category, second_product):
+    assert first_category.product_count == 2
+    first_category.products = second_product
     assert first_category.product_count == 3
+
+
+
--- tests/test_product.py
+++ tests/test_product.py
@@ -1,5 +1,23 @@
+from src.product import Product
+
 def test_product_init(first_product):
     assert first_product.name == "first product"
     assert first_product.description == "first product is a first product"
     assert first_product.price == 3.0
     assert first_product.quantity == 10
+
+def test_new_product_create():
+    new_product = Product.new_product(
+        {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 180000.0,
+         "quantity": 5})
+    new_product.name = "Samsung Galaxy S23 Ultra"
+    new_product.description = "256GB, Серый цвет, 200MP камера"
+    new_product.price = 180000.0
+    new_product.quantity = 10
+
+def price_update(capsys, first_product):
+    first_product.price = 0
+    message = capsys.readouterr()
+    assert  message.out.strip() == 'Цена не должна быть нулевая или отрицательная'
+
+
